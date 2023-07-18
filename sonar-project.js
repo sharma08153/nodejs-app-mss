4ad73926c28098581573610e0193ca6cad934ced
sharma08153 <123318012+sharma08153@users.noreply.github.com>
2023-07-18 19:04:11 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.110.128.180:9000/',
+  serverUrl: 'http://65.0.124.179:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_025d7d0363bf087ab8ceb8bca319243ad33d5e4d',
+	    'sonar.login': 'squ_6a5c975e4791cc8c80f9f06f3ce7f9f2ca6739aa',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
